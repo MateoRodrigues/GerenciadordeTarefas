53c4277c38f45a044d68a8cf3dfc16d2a88568e6
MateoRodrigues <matheus.machado.curso@gmail.com>
2025-04-24 20:00:05 -0300

    navigationbar colocado na janela principal

--- src/main.py
+++ src/main.py
@@ -3,19 +3,34 @@ import flet as ft
 
 def main(page: ft.Page):
     page.title = 'Gerenciador de tarefas'
-    page.theme = "light"
+    page.vertical_alignment = ft.MainAxisAlignment.START
+    page.window_height = 600
+    page.window_width = 600
     page.update()
     page.navigation_bar = ft.NavigationBar(
         destinations=[
-            ft.NavigationBarDestination(icon=ft.Icons.EXPLORE, label="Explore"),
-            ft.NavigationBarDestination(icon=ft.Icons.COMMUTE, label="Commute"),
+            ft.NavigationBarDestination(icon=ft.Icons.HOME, label="Ínicio"),
+            ft.NavigationBarDestination(icon=ft.Icons.DOORBELL_OUTLINED, label="Lembretes"),
             ft.NavigationBarDestination(
-                icon=ft.Icons.BOOKMARK_BORDER,
-                selected_icon=ft.Icons.BOOKMARK,
-                label="Explore",
+                icon=ft.Icons.FEATURED_PLAY_LIST,
+                selected_icon=ft.Icons.FEATURED_PLAY_LIST,
+                label="Projetos",
+            ),
+            ft.NavigationBarDestination(
+                icon=ft.Icons.SETTINGS,
+                selected_icon=ft.Icons.SETTINGS,
+                label="Configurações",
+            ),
+            ft.NavigationBarDestination(
+                icon=ft.Icons.PERSON,
+                selected_icon=ft.Icons.PERSON,
+                label="Perfil",
             ),
         ]
     )
+    page.add(ft.Text("Tarefas!"))
+
+
 
     
     
