13faf38bbe9789d7cc4586a2f6f95821ba51ae03
MateoRodrigues <matheus.machado.curso@gmail.com>
2025-04-24 17:25:57 -0300

    colocando em modo light

--- src/main.py
+++ src/main.py
@@ -3,7 +3,21 @@ import flet as ft
 
 def main(page: ft.Page):
     page.title = 'Gerenciador de tarefas'
+    page.theme = "light"
+    page.update()
+    page.navigation_bar = ft.NavigationBar(
+        destinations=[
+            ft.NavigationBarDestination(icon=ft.Icons.EXPLORE, label="Explore"),
+            ft.NavigationBarDestination(icon=ft.Icons.COMMUTE, label="Commute"),
+            ft.NavigationBarDestination(
+                icon=ft.Icons.BOOKMARK_BORDER,
+                selected_icon=ft.Icons.BOOKMARK,
+                label="Explore",
+            ),
+        ]
+    )
+    page.update()
     
     
 
-ft.app(main)
+ft.app(target=main)
