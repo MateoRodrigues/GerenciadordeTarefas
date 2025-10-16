3cdcd75cdd71bb1e6fac302537cfe2dfb0f25567
MateoRodrigues <matheus.machado.curso@gmail.com>
2025-10-16 19:23:26 -0300

    testando a desesrialização dos arquivos json

--- src/main.py
+++ src/main.py
@@ -1,4 +1,13 @@
-import flet as ft
-from views.pages.pagemain import main
+#import flet as ft
+#from views.pages.pagemain import main
+import json
+from repository.filejson import FileJson
+from pathlib import Path
+fljson = FileJson()
+data = fljson.read()
+for item in data:
+    for key,value in item.items():
+        print(f'{key}: {value}')
+    print('-'*30)
 
-ft.app(target=main)
+#ft.app(target=main)
